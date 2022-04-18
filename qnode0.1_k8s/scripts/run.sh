ebd6f2d657963543d226fa9242fb1c8dfa21d62c
smartquailDev <smartquail.dev@gmail.com>
2022-04-18 03:58:51 -0500

    sh

--- qnode0.1_k8s/scripts/run.sh
+++ qnode0.1_k8s/scripts/run.sh
@@ -8,6 +8,6 @@ python manage.py migrate --noinput
 python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
 python manage.py collectstatic --noinput 
 
-gunicorn --worker-tmp-dir /dev/shm qnode01_app.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+gunicorn --bind "0.0.0.0:${APP_PORT}"  qnode01_app.wsgi:application 
 
 #uwsgi --socket :9000 --workers 4 --master --enable-threads --module qnode0_app.wsgi
