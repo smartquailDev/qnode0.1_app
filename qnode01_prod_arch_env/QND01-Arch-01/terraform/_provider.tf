2004473c184a29932b17ec6a1f5d607fde6eeaf7
smartquail <smartquail@MacBook-Air-de-smartquail.local>
2022-11-15 02:11:10 -0500

    inicial

--- qnode01_prod_arch_env/QND01-Arch-01/terraform/_provider.tf
+++ qnode01_prod_arch_env/QND01-Arch-01/terraform/_provider.tf
@@ -1,6 +1,6 @@
 variable "digitalocean_token" {
   description = "Digital Ocean Token:qnd01"
-  default     = "dop_v1_a343ce0ca32562759328e9fab532aec3704912085a6ba3f65f4a4ce2d2bab719"
+  default     = "dop_v1_5dd9ecff06d66794279ff7b3bbee204e6462f7bc50c4d002f606432f5a745f2d"
 }
 
 variable "spaces_access_id" {
